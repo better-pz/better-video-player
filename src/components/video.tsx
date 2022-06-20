5920c980535e0feda2730a76293e2e20e8f88681
Better <pzeng08@163.com>
2022-06-20 20:57:31 +0800

    update

--- src/components/video.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function video() {
-  return (
-    <video></video>
-  )
-}
---
+++ src/components/video/video.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function video () {
+  return (
+    <>
+      <video controls width="250">
+
+
+      </video>
+    </>
+  )
+}
