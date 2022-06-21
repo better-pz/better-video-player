0787fa3129d3f8aae5cba1da48185cb6a241fc8b
Better <pzeng08@163.com>
2022-06-21 20:40:29 +0800

    update config

---
+++ src/video/video.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function video () {
+  return (
+    <>
+      <video controls width="1000" src="//lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/byted-player-videos/1.0.0/xgplayer-demo-720p.mp4">
+      </video>
+    </>
+  )
+}
